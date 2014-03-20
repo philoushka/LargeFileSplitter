8d800a4e7cf77d49c587f59138345ce5595f7818
Phil Campbell <philoushka@gmail.com>
2014-03-20 10:03:50 -0700

    add string extension class. include FormatWith()

---
+++ Console/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargeFileSplitter
+{
+    public static class StringExtensions
+    {
+        public static string FormatWith(this string input, params object[] args)
+        {
+            if (input == null)
+                throw new ArgumentNullException("format");
+
+            return string.Format(input, args);
+        }
+
+    }
+}
